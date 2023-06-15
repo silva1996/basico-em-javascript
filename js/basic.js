79efff37ff55f226b04b8e0f8ecdd20e1b25fd11
silva199630 <marcosj.antonio3@gmail.com>
2023-06-15 17:23:47 -0300

    envio

---
+++ js/basic.js
@@ -0,0 +1,12 @@
+console.log('olá, javascript!')
+
+var userName = 'Marcos Junior'
+
+document.getElementById('user-name').innerHTML = userName
+
+
+function boasVindas(){
+    alert('sejam bem vindos')
+}
+
+boasVindas()
